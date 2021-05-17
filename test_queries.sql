642470a6de8c91564c8289b875bcd26e9c2c33fa
alfox10 <arturo.milesi@gmail.com>
2021-05-18 01:04:33 +0200

    test queries changed

--- test_queries.sql
+++ test_queries.sql
@@ -1,14 +1,4 @@
 -- SQLite
 
---insert into items (description, effect) VALUES ('','');
-update items set icon='rings' where id=1;
-update items set icon='sword' where id=2;
-update items set icon='sword' where id=3;
-update items set icon='ingots' where id=4;
-update items set icon='sword' where id=5;
-update items set icon='gem' where id=6;
-update items set icon='axe' where id=7;
-update items set icon='belts' where id=8;
-update items set icon='helmets' where id=9;
-update items set icon='helmets' where id=10;
-select id, icon from items;
+
+select * from items;
